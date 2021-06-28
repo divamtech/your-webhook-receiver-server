4a5d41155befecd7661e01d64d82b4fa07eea241
Gaurav Sharma <er.gauravds@gmail.com>
2021-06-28 17:55:55 +0530

    minor updates

--- database/seeds/WebhookSeeder.js
+++ database/seeds/WebhookSeeder.js
@@ -19,7 +19,7 @@ class WebhookSeeder {
 				name: 'Webhook A',
 				slug: 'a-c4f9622f-8456-4dfb-ae3a-4d2ae01a7fc6',
 				auth: 'none',
-				is_active: true
+				is_active: true,
 			},
 			{
 				name: 'Webhook B',
@@ -27,14 +27,14 @@ class WebhookSeeder {
 				auth: 'basic',
 				auth_key1: 'userX',
 				auth_key2: 'passwordY',
-				is_active: true
+				is_active: true,
 			},
 			{
 				name: 'Webhook C',
 				slug: 'c-c4f9622f-8456-4dfb-ae3a-4d2ae01a7fc6',
 				auth: 'bearer',
 				auth_key1: '050b3f5a76caf1778ac389eefb766b872d51b57ed157f49615db251473eddb6c',
-				is_active: true
+				is_active: true,
 			},
 			{
 				name: 'Webhook D',
@@ -42,7 +42,7 @@ class WebhookSeeder {
 				auth: 'HMAC-SHA1',
 				auth_key1: '3da541559918a808c2402bba5012f6c60b27661c',
 				auth_key2: '92429d82a41e930486c6de5ebda9602d55c39986',
-				is_active: true
+				is_active: true,
 			},
 			{
 				name: 'Webhook E',
@@ -50,7 +50,7 @@ class WebhookSeeder {
 				auth: 'HMAC-SHA256',
 				auth_key1: 'affff7ff15ea7649eaf95ddc119db9a4752abbb40c7af701e06275a1fe3c690d',
 				auth_key2: '050b3f5a76caf1778ac389eefb766b872d51b57ed157f49615db251473eddb6c',
-				is_active: true
+				is_active: true,
 			},
 			{
 				name: 'Events',
@@ -58,8 +58,8 @@ class WebhookSeeder {
 				auth: 'basic',
 				auth_key1: 'userX',
 				auth_key2: 'passwordY',
-				is_active: true
-			}
+				is_active: true,
+			},
 		]);
 	}
 }
